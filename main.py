5641e97f3608deda984081813ee35cfa4f937fe2
JatobaMaster <82157105+JatobaMaster@users.noreply.github.com>
2022-08-30 12:32:53 -0300

    Update main.py
    Dei uma mudada na estrutura do código, mas na aula de filosofia.

--- main.py
+++ main.py
@@ -1,43 +1,70 @@
 from manim import *
 
-class Cônicas(Scene):
+
+class t(Scene):
     def construct(self):
+        
+        plane = NumberPlane(
+        axis_config={"include_numbers": True, "include_tip": True
+        }
+        )
 
-        plano_cartesiano = NumberPlane(axis_config={"include_numbers": True, "include_tip": True})
-        legenda = plano_cartesiano.get_axis_labels(x_label=MathTex(r"\\x"), y_label=MathTex(r"\\y"))
+        label = plane.get_axis_labels(
+        x_label=MathTex(r"\\x"), 
+        y_label=MathTex(r"\\y")
+        )
 
-        ponto = Dot()
-        ponto.move_to([0, 0, 0])
+        self.add(plane, label)
+        self.wait(1.5)
 
-        circulo = Circle(radius=2, color=WHITE, fill_opacity=0.5)
-        circulo.move_to([0, 0, 0])
+        circle = Circle(
+        radius=1,
+        color=RED_C,
+        fill_opacity=0.5
+        )
+        circle.move_to([0, 0, 0])
 
-        elipse = Ellipse(width=4, height=2, color=WHITE, fill_opacity=0.5)
-        elipse.move_to([0, 0, 0])
+        self.play(GrowFromCenter(circle), run_time=2)
+        self.wait(1.5)
+        self.play(ShrinkToCenter(circle))
 
-        hipérbole = ImplicitFunction(lambda x, y: x**2 - y**2 -4, color=WHITE)
+        ellipse = Ellipse(
+        width=2,
+        height=1,
+        color=GREEN_C,
+        fill_opacity=0.5
+        )
+        ellipse.move_to([0, 0, 0])
+
+        self.play(Transform(circle, ellipse))
+        self.wait(1.5)
+        self.play(ShrinkToCenter(circle))
+
+        hyperbole = ImplicitFunction(
+        lambda x, y: x**2 - y**2 -4,
+        color=BLUE_C
+        )
+
+        self.play(Transform(circle, hyperbole))
+        self.wait(1.5)
+        self.play(ShrinkToCenter(circle))
 
         def func(x):
             return 0.3*(x)**2 -1
-        parábola = plano_cartesiano.plot(func, color=WHITE) 
+        parabole = plane.plot(func,
+        color=YELLOW_C
+        )
+
+        dot = Dot()
+
+        self.play(Transform(circle, parabole))
+        self.wait(1.5)
+        self.play(Transform(circle, dot))
+
+        self.play(FadeOut(circle, plane, label))
 
         logo = ManimBanner()
 
-        self.wait(1)
-        self.play(Create(plano_cartesiano), run_time=2)
-        self.play(Write(legenda))
-        self.play(GrowFromCenter(circulo), run_time=2)
-        self.wait(1)
-        self.play(Transform(circulo, ponto))
-        self.play(Transform(circulo, elipse), run_time=2)
-        self.wait(1)
-        self.play(Transform(circulo, ponto))
-        self.play(Transform(circulo, hipérbole), run_time=2)
-        self.wait(1)
-        self.play(Transform(circulo, ponto))
-        self.play(Transform(circulo, parábola), run_time=2)
-        self.wait(1)
-        self.play(FadeOut(plano_cartesiano, legenda, circulo))
         self.play(logo.create())
         self.play(logo.expand())
         self.wait(1)
